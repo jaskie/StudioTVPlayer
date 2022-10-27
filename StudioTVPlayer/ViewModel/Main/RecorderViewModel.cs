fa390469bfcd07e99fd6f03527b676b99a546f5c
Jerzy Jaśkiewicz <jurek.jaskiewicz@gmail.com>
2022-10-27 21:44:57 +0200

    wip: RecorderViewModel

--- StudioTVPlayer/ViewModel/Main/RecorderViewModel.cs
+++ StudioTVPlayer/ViewModel/Main/RecorderViewModel.cs
@@ -1,25 +1,56 @@
 ﻿using StudioTVPlayer.Providers;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 
 namespace StudioTVPlayer.ViewModel.Main
 {
-    public class RecorderViewModel : RemovableViewModelBase
+    public class RecorderViewModel : RemovableViewModelBase, IDataErrorInfo
     {
         private Model.InputBase _input;
+        private string _fileName;
+        private string _folder;
+        private string _fullPath;
 
         public RecorderViewModel(Model.InputBase input)
         {
             _input = input;
         }
 
-        public string Folder { get; set; }
+        public string Folder
+        {
+            get => _folder;
+            set
+            {
+                if (!Set(ref _folder, value))
+                    return;
+                SetNewFullPath();
+            }
+        }
 
-        public string FileName { get; set; }
+        public string FileName
+        {
+            get => _fileName;
+            set
+            {
+                if (!Set(ref _fileName, value))
+                    return;
+                SetNewFullPath();
+            }
+        }
 
         public Model.EncoderPreset EncoderPreset { get; set; }
 
+        public bool IsRecording { get; set; }
+
+        public bool CanStartRecord => IsRecording || CanStartRecording();
+
         public static IEnumerable<Model.EncoderPreset> EncoderPresets => GlobalApplicationData.Current.EncoderPresets;
+
+        public string Error => string.Empty;
+
+        public string this[string columnName] => ReadErrorInfo(columnName);
 
         public override void Apply()
         {
@@ -30,6 +61,29 @@ namespace StudioTVPlayer.ViewModel.Main
         {
             return true;
         }
+
+        protected override bool CanRequestRemove(object obj)
+        {
+            return !IsRecording;
+        }
+
+        private string ReadErrorInfo(string propertyName)
+        {
+            return string.Empty;
+        }
+
+        private void SetNewFullPath()
+        {
+            _fullPath = Path.Combine(Folder, FileName);
+            NotifyPropertyChanged(nameof(CanStartRecord));
+        }
+
+        private bool CanStartRecording()
+        {
+            return Directory.Exists(Folder) && !File.Exists(_fullPath);
+        }
+
+
 
     }
 }
